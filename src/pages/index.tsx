da9497543decfbffe5ed7cf252b3455e5c526590
Leon von dem Berge <leon.vondemberge@gmail.com>
2019-02-10 18:18:17 +0200

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 interface IProps { }
@@ -11,17 +8,15 @@ class IndexPage extends React.Component<IProps, {}>{
 
   render() {
     return (
-      <Layout>
+      <React.Fragment>
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
         <h1>Hi people </h1>
-        < p > Welcome to your new Gatsby site.</p>
+        < p > This is Leon's Gatsby TypeScript Starter.</p>
         < p > Now go build something great.</p>
         < div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }
         }>
-          <Image />
         </div>
-        < Link to="/page-2/" > Go to page 2 </Link>
-      </Layout>
+      </React.Fragment>
     )
   }
 
